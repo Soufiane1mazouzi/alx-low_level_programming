fe1e3b5a87188996fb37eb472ff06eeb2478fb5b
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2023-10-16 04:45:05 -0700

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,32 +8,32 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	while (s[i])
-	{
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			break;
-		}
-		i++;
-	}
+    while (s[i])
+    {
+        if (s[i] == '-')
+        {
+            sign *= -1;
+        }
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            break;
+        }
+        i++;
+    }
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (result > (INT_MAX - (s[i] - '0')) / 10)
-		{
-			return ((sign == 1) ? INT_MAX : INT_MIN);
-		}
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        if (result > (INT_MAX - (s[i] - '0')) / 10)
+        {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
 
-	return ((result * sign);
+    return (result * sign);
 }
