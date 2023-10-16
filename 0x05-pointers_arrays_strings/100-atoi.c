761eb62f364f8236d93a977e24b371efd8fb3764
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2023-10-16 04:19:40 -0700

    task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <limits.h>  /* Include for INT_MAX and INT_MIN */
+#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -8,39 +8,32 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1; /* Initialize sign to positive */
+	int sign = 1;
 	int result = 0;
 	int i = 0;
 
-	/* Handle leading spaces and signs */
-	while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
+	while (s[i])
 	{
 		if (s[i] == '-')
 		{
-			sign *= -1; /* Handle negative sign */
+			sign *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			break;
 		}
 		i++;
 	}
 
-	/* Iterate through the string to convert to an integer */
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		/* Check for integer overflow */
 		if (result > (INT_MAX - (s[i] - '0')) / 10)
 		{
-			if (sign == 1)
-			{
-				return INT_MAX;
-			}
-			else
-			{
-				return INT_MIN;
-			}
+			return (sign == 1) ? INT_MAX : INT_MIN;
 		}
 		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
-	/* Apply the sign */
-	return result * sign;
+	return (result * sign);
 }
