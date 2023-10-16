444a02247b82e03e5f9079d45033f57ef20ebec1
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2023-10-16 04:04:23 -0700

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,20 +7,20 @@
  */
 void puts_half(char *str)
 {
-    int length = 0;
-    int i;
-    int start;
+	int length = 0;
+	int i;
+	int start;
 
-    /* Calculate the length of the string */
-    while (str[length] != '\0')
-        length++;
+	/* Calculate the length of the string */
+	while (str[length] != '\0')
+		length++;
 
-    /* Calculate the starting index for the second half */
-    start = (length + 1) / 2;
+	/* Calculate the starting index for the second half */
+	start = (length + 1) / 2;
 
-    /* Print the second half of the string */
-    for (i = start; i < length; i++)
-        _putchar(str[i]);
+	/* Print the second half of the string */
+	for (i = start; i < length; i++)
+		_putchar(str[i]);
 
-    _putchar('\n');
+	_putchar('\n');
 }
