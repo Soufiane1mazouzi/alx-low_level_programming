4968d2597faa4dd119c1dcb276f9a5e14d6b4375
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2023-10-16 03:40:17 -0700

    taskpointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,16 @@
-#include <stdio.h>
+#ifndef main_h
+#define main_h
 
-void reset_to_98(int *);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _putchar(char c);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
-
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("%d\n", n);
-	return (0);
-}
+#endif
