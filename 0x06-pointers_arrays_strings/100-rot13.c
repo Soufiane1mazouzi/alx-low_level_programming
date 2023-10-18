25059c57ae7c26c80f3579d81d1f4edf9b32c65d
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2023-10-18 15:07:23 -0700

    more

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char *rot13(char *s)
+{
+    char *ptr = s;
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        char c = s[i];
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+            char base = (c >= 'a' && c <= 'z') ? 'a' : 'A';
+            *ptr = (c - base + 13) % 26 + base;
+        }
+        else
+        {
+            *ptr = c;
+        }
+        ptr++;
+    }
+
+    return s;
+}
