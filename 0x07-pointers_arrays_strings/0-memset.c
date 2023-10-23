48778e599400a37abbe0ae773ea8f2bedaf6d582
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2023-10-23 07:40:15 -0700

    more

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,35 @@
 #include "main.h"
 
-/**
- * _memset - Fills the first n bytes of the memory area pointed to by s
- * with the constant byte b.
- * @s: Pointer to the memory area to fill.
- * @b: The constant byte to fill the memory with.
- * @n: Number of bytes to fill.
- *
- * Return: A pointer to the memory area s.
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-    unsigned int i;
+int main(void) {
+    int rows = 5;
+    int cols = 10;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
+    char **arr = (char **)malloc(rows * sizeof(char *));
+
+    for (int i = 0; i < rows; i++) {
+        arr[i] = (char *)malloc(cols * sizeof(char));
     }
 
-    return s;
+    // Fill the 2D array with some data
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr[i][j] = 'A' + (i % 26);
+        }
+    }
+
+    // Use _putchar to print all the elements of the 2D array
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            _putchar(arr[i][j]);
+        }
+        _putchar('\n');
+    }
+
+    // Free the allocated memory
+    for (int i = 0; i < rows; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+
+    return (0);
 }
