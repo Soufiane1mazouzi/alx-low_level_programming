e440483320ac7bcff14b0c697c628aa39235590f
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2023-10-23 13:44:12 -0700

    pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,7 @@
 /* Function prototype for _memset */
 char *_memset(char *s, char b, unsigned int n);
 
+/* Function prototype for print_chessboard */
+void print_chessboard(char (*a)[8]);
+
 #endif /* MAIN_H */
