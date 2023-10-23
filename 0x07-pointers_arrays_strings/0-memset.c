33f7da43fa54afd27ed177a1398274615244051e
Soufiane1mazouzi <mazouzi.soufiane124@gmail.com>
2023-10-23 07:47:04 -0700

    more

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,35 +1,10 @@
 #include "main.h"
 
-int main(void) {
-    int rows = 5;
-    int cols = 10;
-
-    char **arr = (char **)malloc(rows * sizeof(char *));
-
-    for (int i = 0; i < rows; i++) {
-        arr[i] = (char *)malloc(cols * sizeof(char));
-    }
-
-    // Fill the 2D array with some data
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            arr[i][j] = 'A' + (i % 26);
-        }
-    }
-
-    // Use _putchar to print all the elements of the 2D array
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            _putchar(arr[i][j]);
-        }
-        _putchar('\n');
-    }
-
-    // Free the allocated memory
-    for (int i = 0; i < rows; i++) {
-        free(arr[i]);
-    }
-    free(arr);
-
-    return (0);
+char *_memset(char *s, char b, unsigned int n)
+{
+  while (n--) {
+    *s = b;
+    s++;
+  }
+  return s;
 }
